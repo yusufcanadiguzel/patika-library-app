f59d883f48882f1d0405743267adb17d8632eb46
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-09-21 20:03:27 +0300

    Initial Create

---
+++ LibraryApp.Entities/Concrete/Author.cs
@@ -0,0 +1,17 @@
+﻿using LibraryApp.Entities.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Entities.Concrete
+{
+    public class Author : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ LibraryApp.Entities/Concrete/Book.cs
@@ -0,0 +1,20 @@
+﻿using LibraryApp.Entities.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Entities.Concrete
+{
+    public class Book : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int AuthorId { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public DateTime PublishDate { get; set; }
+        public string ISBN { get; set; } = string.Empty;
+        public int CopiesAvailable { get; set; }
+    }
+}
